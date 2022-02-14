c458f5a6980c1334b52c849596c5d5cc6e4c6d56
Gabit <gbzh2020gmail.com>
2022-02-15 00:15:30 +0600

    add Eslint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //import _ from "./lodash";
 import _ from "lodash";
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
 
-console.log("Hello, Hexlet!")
+console.log("Hello, Hexlet!");
+
+const find_sum = (a, b) => {
+  const c = a + b;
+  return c;
+};
